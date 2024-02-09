ba6fd078b78178c56e5666c20e17cd62f60bc67a
Bruno Campoi <brunocampoi31@gmail.com>
2024-02-09 11:31:53 +0100

    feat: allows saving the results
    Introduce the function to save the results back to the Google spreadsheet. Minor changes to the README file.

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Python application that reads a Google spreadsheet, calculate the columns "Situação" and "Nota para Aprovação Final" based on the data and finally write it back.
 
-This project comprises the programming challenge proposed by Tunts.Rocks and was done in the Python language.
+This project comprises the programming challenge proposed by Tunts.Rocks and was done in the Python programming language.
 
 ## Set Up environment
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the Google client library for Python.
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the Google client library for Python. To do this, open the terminal and type:
 
 ```bash
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
@@ -16,16 +16,9 @@ Set up the credentials for access Google Sheets following the [instructions](htt
 At the end of this process you should have the "credentials.json" file in the working directory and the Google client library for Python installed.
 
 ## Usage
+To use this script, just open the terminal and run the following code:
 
-```python
-import foobar
-
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
+```bash
+python3 chalenge_students.py
 ```
+The script will return log lines to monitor the application's activities and the results can be checked in the spreadsheet.
--- chalenge_students.py
+++ chalenge_students.py
@@ -9,55 +9,64 @@ from googleapiclient.errors import HttpError
 
 
 def main():
-  # the spreadsheet ID can be found in the spreadsheet URL
-  values = getData("10FdFjl30y7ZI6DQ7so1MTQzf8sIXUptUPLGwPzbOjoU","A2:F27")
+  # the spreadsheet ID can be found in the URL of the document
+  spreadsheetId = "10FdFjl30y7ZI6DQ7so1MTQzf8sIXUptUPLGwPzbOjoU"
 
-  if not values:
-      print("No data found.")
-      return
+  # read then values from the Google spreadsheet
+  try:
+    spreadsheet_values = getData(spreadsheetId,"A2:F27")
+  except UnboundLocalError as err:
+    print("The spreadsheet could not be accessed.")
+    return
   
   # find the total number of classes
-  total_classes = int(values[0][0].split()[-1])
+  total_classes = int(spreadsheet_values[0][0].split()[-1])
 
-  # iterates over the students
-  for i in range(2, len(values)):
-    reg_number = values[i][0]
-    name = values[i][1]
-    num_absences = int(values[i][2])
+  # iterates over the students rows
+  for i in range(2, len(spreadsheet_values)):
+    reg_number = spreadsheet_values[i][0]
+    name = spreadsheet_values[i][1]
+    num_absences = int(spreadsheet_values[i][2])
 
-    # line LOG
-    print(f"\n LOG: {values[i]}")
+    # row LOG
+    print(f"\n LOG: {spreadsheet_values[i]}")
 
     # average grande between the 3 tests in a scale from 0 to 10
-    avg = (int(values[i][3])+int(values[i][4])+int(values[i][5]))/30
+    avg = (int(spreadsheet_values[i][3])+int(spreadsheet_values[i][4])+int(spreadsheet_values[i][5]))/30
     print(f" avg -> {str(avg)}")
     
     # verify the situation of the student
     if(num_absences/total_classes > 0.25):
-      values[i].append("Reprovado por Falta")
-      values[i].append("0")
+      spreadsheet_values[i].append("Reprovado por Falta")
+      spreadsheet_values[i].append("0")
+      print(" situation -> Reprovado por Falta")
     elif(avg<5):
-      values[i].append("Reprovado por Nota")
-      values[i].append("0")
+      spreadsheet_values[i].append("Reprovado por Nota")
+      spreadsheet_values[i].append("0")
+      print(" situation ->Reprovado por Nota")
     elif(avg>=7):
-      values[i].append("Aprovado")
-      values[i].append("0")
+      spreadsheet_values[i].append("Aprovado")
+      spreadsheet_values[i].append("0")
+      print(" situation -> Aprovado")
     elif(avg>=5 and avg<7):
-      values[i].append("Exame Final")
+      spreadsheet_values[i].append("Exame Final")
+      print(" situation -> Exame Final ", end="")
       # calculate the minimun grade for approval in the final exam
-      naf = 10-avg
-      values[i].append(math.ceil(naf))
-      print(f" naf -> {math.ceil(naf)} ")
+      naf = math.ceil(10-avg)
+      spreadsheet_values[i].append(naf)
+      print(f"(naf -> {naf})")
     else:
-      values[i].append("ERROR!")
+      spreadsheet_values[i].append("ERROR!")
 
-      
-  #update_values(
-  #    "1CM29gwKIzeXsAppeNwrc8lbYaVMmUclprLuLYuHog4k",
-  #    "A1:C2",
-  #    "USER_ENTERED",
-  #    [["A", "B"], ["C", "D"]],
-  #)
+  results_list = [row[6:] for row in spreadsheet_values[2:]]
+
+  # write the results in the Google spreadsheet
+  update_values(
+      spreadsheetId,
+      "G4:H27",
+      "USER_ENTERED",
+      results_list
+  )
 
 
 
@@ -67,12 +76,13 @@ def getData(spreadsheetId: str, range: str):
   """
 
   # If modifying these scopes, delete the file token.json.
-  SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+  SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
   
-  creds = None
+  
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
+  creds = None
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
@@ -107,23 +117,35 @@ def getData(spreadsheetId: str, range: str):
 
 
 
-
-"""def update_values(spreadsheet_id, range_name, value_input_option, _values):
+def update_values(spreadsheet_id, range_name, value_input_option, values):
   "Update the values in a specific range of cells in the spreadsheet."
   
+  # If modifying these scopes, delete the file token.json.
   SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-  creds= Credentials.from_authorized_user_file("token.json", SCOPES)
-  # pylint: disable=maybe-no-member
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  creds = None
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  # update the values in the spreadsheet
   try:
     service = build("sheets", "v4", credentials=creds)
-    values = [
-        [
-            # Cell values ...
-        ],
-        # Additional rows ...
-    ]
-    body = {"values": values}
+    body = {"majorDimension":"ROWS", "values": values}
     result = (
         service.spreadsheets()
         .values()
@@ -131,18 +153,15 @@ def getData(spreadsheetId: str, range: str):
             spreadsheetId=spreadsheet_id,
             range=range_name,
             valueInputOption=value_input_option,
-            body=body,
+            body=body
         )
         .execute()
     )
     print(f"{result.get('updatedCells')} cells updated.")
     return result
   except HttpError as error:
-    print(f"An error occurred: {error}")
+    print(f"An error occurred while updating the spreadsheet: {error}")
     return error
-"""
-
-
 
 
 
